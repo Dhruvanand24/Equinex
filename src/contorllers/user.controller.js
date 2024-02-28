bd381e13819e861652819375ee832c215b9fae14
Dhruv_Anand <ananddhruv475@gmail.com>
2024-02-28 14:26:45 +0530

    a

--- src/contorllers/user.controller.js
+++ src/contorllers/user.controller.js
@@ -1,15 +1,13 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js";
 import {User} from "../models/user.model.js";
-import {uploadOnCloudinary} from "../utils/cloudinary.js";
+// import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req,res)=>{
      // get user details from frontend
      // validation - not empty
      // check if user already exists: username, email
-     // check for images, check for avatar
-     // upload them to cloudinary, avatar
      // create user object - create entry in db
      // remove password and refresh token field from response
      // check for user creation 
