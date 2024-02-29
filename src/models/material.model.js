cf96fa6105fced076a89fb4a5468e1038553fb91
Dhruv Anand <89727117+Dhruvanand24@users.noreply.github.com>
2024-02-29 13:03:02 +0530

    added inventory, item and material schema (#5)

---
+++ src/models/material.model.js
@@ -0,0 +1,16 @@
+import mongoose, {Schema} from "mongoose";
+
+
+const materialSchema = new Schema({
+    Name: {
+        type: String, 
+        required: true
+    },
+    description: {
+        type: Object, 
+    }
+})
+
+
+
+export const Material = mongoose.model("Material", materialSchema)
