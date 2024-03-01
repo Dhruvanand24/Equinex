eb5f66990498b1dc56f3f245b3a6aed84fbb71d4
MD KAIF MANZAR <103236678+kaifmanzar26MAR@users.noreply.github.com>
2024-03-01 12:16:49 +0530

    added creating of material, material request, getting all material, all material request (#12)

--- src/controllers/material.controller.js
+++ src/controllers/material.controller.js
@@ -2,27 +2,86 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Material } from "../models/material.model.js";
-
+import { MaterialRequest } from "../models/materialRequest.model.js";
 const createMaterial = asyncHandler(async (req, res) => {
-  const { name, description } = req.body;
-  if ([name].some((field) => field?.trim() === "")) {
+  const { Name, description } = req.body;
+
+  if ([Name].some((field) => field?.trim() === "")) {
     throw new ApiError(400, "Name Required");
   }
+
   const existedMaterial = await Material.findOne({
-    name,
+    Name,
   });
+
   if (existedMaterial) {
-    throw new ApiError(409, "Material with same name already exists");
+    throw new ApiError(409, "Material with the same name already exists");
   }
+
   const material = await Material.create({
-    name,
+    Name,
     description,
   });
-  if (!material) {
-    throw new ApiError(500, "Something Went wrong while creating material");
+
+  if (material) {
+    return res
+      .status(201)
+      .json(new ApiResponse(200, material, "Material created successfully")); // Fix here, use `material` instead of `item`
+  }
+
+  throw new ApiError(500, "Something went wrong while creating material");
+});
+
+const createMaterialRequest = asyncHandler(async (req, res) => {
+  const {
+    Order_Id,
+    Date_of_request,
+    List_of_materials,
+    Status_approval,
+    Department_requrest_raise,
+    Store,
+    Receive_in_Department,
+  } = req.body;
+
+  const newrequest = await MaterialRequest.create({
+    Order_Id,
+    Date_of_request,
+    List_of_materials,
+    Status_approval,
+    Department_requrest_raise,
+    Store,
+    Receive_in_Department,
+  });
+
+  if (!newrequest) {
+    throw new ApiError(
+      500,
+      "Something Went wrong while creating material request"
+    );
   }
   return res
     .status(201)
-    .json(new ApiResponse(200, item, "material created successfully"));
+    .json(
+      new ApiResponse(200, newrequest, "material request created successfully")
+    );
 });
-export { createMaterial };
+
+const getAllMaterial = asyncHandler(async (req, res) => {
+  const allMat = await Material.find();
+  if (!allMat) {
+    throw new ApiError(500, "Something Went wrong while getting materials");
+  }
+  return res
+    .status(201)
+    .json(new ApiResponse(200, allMat, "material get successfully"));
+});
+const getAllMaterialRequest = asyncHandler(async (req, res) => {
+  const allMatReq = await MaterialRequest.find();
+  if (!allMatReq) {
+    throw new ApiError(500, "Something Went wrong while getting materials");
+  }
+  return res
+    .status(201)
+    .json(new ApiResponse(200, allMatReq, "material get successfully"));
+});
+export { createMaterial, createMaterialRequest, getAllMaterial, getAllMaterialRequest };
--- src/routes/material.routes.js
+++ src/routes/material.routes.js
@@ -1,7 +1,15 @@
 import { Router } from "express";
-import { createMaterial } from "../controllers/material.controller.js";
+import {
+  createMaterial,
+  createMaterialRequest,
+  getAllMaterial,
+  getAllMaterialRequest,
+} from "../controllers/material.controller.js";
 
 const router = Router();
 router.route("/createMaterial").post(createMaterial);
+router.route("/getallmaterial").get(getAllMaterial);
+router.route("/createMaterialRequest").post(createMaterialRequest);
+router.route("/getallmaterialrequest").get(getAllMaterialRequest);
 
 export default router;
