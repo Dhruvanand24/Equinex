11f12d7f4305cdea808198ef15205839e3a8737b
MD KAIF MANZAR <103236678+kaifmanzar26MAR@users.noreply.github.com>
2024-03-01 14:44:31 +0530

    purchase req creation (#13)
    Co-authored-by: Dhruv Anand <89727117+Dhruvanand24@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import userRouter from "./routes/user.routes.js";
 import homeRouter from "./routes/home.routes.js";
 import itemRouter from "./routes/item.routes.js";
 import materialRouter from "./routes/material.routes.js";
-import inventoryRouter from "./routes/inventory.routes.js";
+import purchaseRouter from "./routes/purchase.routes.js";
 
 const app = express();
 
@@ -28,9 +28,9 @@ app.use(cookieParser());
 //routes declaration
 app.use("/", homeRouter);
 app.use("/api/v1/users", userRouter);
-
 app.use("/api/v1/item", itemRouter);
 app.use("/api/v1/material", materialRouter);
+app.use("/api/v1/purchase", purchaseRouter);
 
 app.use("/api/v1/inventory", inventoryRouter);
 
--- src/models/purchaseRequest.model.js
+++ src/models/purchaseRequest.model.js
@@ -1,25 +1,40 @@
 import mongoose, { Schema } from "mongoose";
 
 const purchase_request_schema = new Schema({
-  Requester_name: {
-    type: String,
+  Requester: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User", // currecnt login user
+  },
+  Date_of_request: {
+    type: Date,
     required: true,
   },
-  List_of_items: {
-    type: Object, //items with quantity
-    required: true,
-  },
+  List_of_materials: [
+    {
+      material_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Material",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
   isApproved: {
     type: Boolean,
     required: true,
   },
   Approved_by: {
-    type: String,
-    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // the user id who have authority to approve
   },
   is_GRN_created: {
     type: Boolean,
     required: true,
+    default: false,
   },
   GRN_data: {
     type: Object, // created by, date, quantity recived, quantity orderd, ... etc view grn slip for refrence
