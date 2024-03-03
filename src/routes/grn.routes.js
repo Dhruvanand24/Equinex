7d22c81d1d255bedb7a1c0278031b8c87f4356f5
MD KAIF MANZAR <103236678+kaifmanzar26MAR@users.noreply.github.com>
2024-03-03 13:38:58 +0530

    qc update done (#18)

--- src/routes/grn.routes.js
+++ src/routes/grn.routes.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 import {
   CreateGRN,
   UpdateReceivedData,
+  UpdateQCData,
 } from "../controllers/grn.controller.js";
 const router = Router();
 router.route("/creategrn").post(CreateGRN);
 router.route("/updatereceiveddata").post(UpdateReceivedData);
+router.route("/updateqcdata").post(UpdateQCData);
 
 export default router;
