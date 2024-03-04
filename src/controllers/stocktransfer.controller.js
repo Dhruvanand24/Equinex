329d426cc33c12bb2cf9a8bfae0b210dae69ecae
Dhruv_Anand <ananddhruv475@gmail.com>
2024-03-04 15:34:36 +0530

    a

--- src/controllers/stocktransfer.controller.js
+++ src/controllers/stocktransfer.controller.js
@@ -3,6 +3,65 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { StockTransferRequest } from "../models/stockTransfer.model.js";
 
-const createStockTransferRequest = asyncHandler(async(req, res)=> {
-    const {material_request_id, List_of_materials, createdby } = req.body;
-})
+const createStockTransferRequest = asyncHandler(async (req, res) => {
+  const { material_request_id, List_of_materials, createdby } = req.body;
+
+  const createdStockTransferRequest = await StockTransferRequest.create({
+    material_request_id,
+    List_of_materials,
+    createdby,
+    Approved_by_warehouse: false,
+    Intransit: false,
+    createdby,
+  });
+  if (!createdStockTransferRequest) {
+    throw new ApiError(
+      500,
+      "Something went wrong while creating the stock transfer request Order"
+    );
+  }
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(
+        200,
+        createdStockTransferRequest,
+        "stock transfer request created successfully"
+      )
+    );
+});
+const updateApproval = asyncHandler(async (req, res) => {
+  const { StockTransferRequestid, status } = req.body;
+
+  StockTransferRequest.findByIdAndUpdate(
+    StockTransferRequestid,
+    {
+      $set: {
+        Approved_by_warehouse: true,
+      },
+    },
+    { new: true }
+  );
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Approved successfully"));
+});
+
+const updateTransit = asyncHandler(async (req, res) => {
+  const { StockTransferRequestid, status } = req.body;
+
+  StockTransferRequest.findByIdAndUpdate(
+    StockTransferRequestid,
+    {
+      $set: {
+        Intransit: true,
+      },
+    },
+    { new: true }
+  );
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Approved successfully"));
+});
+
+export { createStockTransferRequest, updateApproval, updateTransit };
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -101,28 +101,30 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
-const logoutUser = asyncHandler(async (req, res) => {
+const logoutUser = asyncHandler(async(req, res) => {
   await User.findByIdAndUpdate(
-    req.user._id,
-    {
-      $set: {
-        refreshToken: undefined,
+      req.user._id,
+      {
+          $unset: {
+              refreshToken: 1 // this removes the field from document
+          }
       },
-    },
-    {
-      new: true,
-    }
-  );
+      {
+          new: true
+      }
+  )
+
   const options = {
-    httpOnly: true,
-    secure: true,
-  };
+      httpOnly: true,
+      secure: true
+  }
+
   return res
-    .status(200)
-    .clearCookie("accessToken", options)
-    .clearCookie("refreshToken", options)
-    .json(new ApiResponse(200, {}, "user logged Out"));
-});
+  .status(200)
+  .clearCookie("accessToken", options)
+  .clearCookie("refreshToken", options)
+  .json(new ApiResponse(200, {}, "User logged Out"))
+})
 
 const refreshAccessToken = asyncHandler(async (req, res) => {
   const incomingRefreshToken =
