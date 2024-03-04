3e563b44aaf6b4b23ec2e77024a8dd34d3d1d8af
Dhruv_Anand <ananddhruv475@gmail.com>
2024-03-04 19:37:26 +0530

    a

--- src/app.js
+++ src/app.js
@@ -14,16 +14,16 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: 'http://localhost:5173',
     credentials: true,
   })
 );
+
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-
 
 //routes declaration
 app.use("/", homeRouter);
