d60fcc04bf6feb6ad3d4f0a4f0ae3bc5754f8c70
Dhruv_Anand <ananddhruv475@gmail.com>
2024-03-04 19:31:50 +0530

    a

--- src/app.js
+++ src/app.js
@@ -23,10 +23,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
-app.use(cookieParser());
+
 
 //routes declaration
 app.use("/", homeRouter);
