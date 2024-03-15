010b57c4905d834aa3ace0495a513cd536726f17
MD KAIF MANZAR <103236678+kaifmanzar26MAR@users.noreply.github.com>
2024-03-15 11:33:57 +0530

    added get materialinventory by id (#27)

--- src/routes/inventory.routes.js
+++ src/routes/inventory.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   getFullIventory,
+  getMaterialInventoryById,
   getMaterialStatus,
   updateInventory,
 } from "../controllers/inventory.controller.js";
@@ -9,5 +10,6 @@ const router = Router();
 router.route("/getfullinventory").get(getFullIventory);
 router.route("/getmaterialstatus").post(getMaterialStatus);
 router.route("/updateinventory").post(updateInventory);
+router.route("/getmaterialinventorybyid").post(getMaterialInventoryById);
 
 export default router;
